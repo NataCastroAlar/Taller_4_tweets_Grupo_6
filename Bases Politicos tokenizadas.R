a3c7c653f39374062991fbd90e98979b39cc40a2
NataliaCastroAlarcon <castronatal@unaindes.edu.co>
2023-03-16 23:22:02 -0600

    Update Bases Politicos tokenizadas.R

--- Bases Politicos tokenizadas.R
+++ Bases Politicos tokenizadas.R
@@ -1,182 +1,111 @@
-rm(list = ls())
-require("pacman")
-library(readr)
 
-require("pacman")
+###GRUPO 6###
+###VICTOR CHIQUE, VICTOR SANHEZ, NATALIA CASTRO
+
+rm(list = ls())
+
+library(pacman) 
 
 # Cargar las librerías listadas e instalarlas en caso de ser necesario
 p_load(tidyverse, # Manipular dataframes
        tm,   # para Text Mining
        tidytext, #Para tokenización
        wordcloud, # Nube de palabras 
-       SentimentAnalysis #Análisis de sentimientos 
+       SentimentAnalysis, #Análisis de sentimientos,
+       topicmodels
 ) 
 
 
 train <- read_csv("Documents/UNIANDES BIG DATA MACHINE LEARNING/Archivos R/DATOS TALLERES/DATOS POLITICOS/train.csv")
 test <- read_csv("Documents/UNIANDES BIG DATA MACHINE LEARNING/Archivos R/DATOS TALLERES/DATOS POLITICOS/test.csv")
 
-
-##-----------------------Limpieza de texto y tokenización---------------------##
-
-
-limpiar_tokenizar <- function(text){
-  # El orden de la limpieza no es arbitrario
-  # Se convierte todo el text a minúsculas
-  nuevo_text <- tolower(text)
-  # Eliminación de páginas web (palabras que empiezan por "http." seguidas 
-  # de cualquier cosa que no sea un espacio)
-  nuevo_text <- str_replace_all(nuevo_text,"http\\S*", "")
-  # Eliminación de signos de puntuación
-  nuevo_text <- str_replace_all(nuevo_text,"[[:punct:]]", " ")
-  # Eliminación de números
-  nuevo_text <- str_replace_all(nuevo_text,"[[:digit:]]", " ")
-  # Eliminación de espacios en blanco múltiples
-  nuevo_text <- str_replace_all(nuevo_text,"[\\s]+", " ")
-  # Tokenización por palabras individuales
-  nuevo_text <- str_split(nuevo_text, " ")[[1]]
-  # Eliminación de tokens con una longitud < 2
-  nuevo_text <- keep(.x = nuevo_text, .p = function(x){str_length(x) > 1})
-  return(nuevo_text)
-}
-#Descartamos NAs
-
-sapply(train, function(x) sum(is.na(x)))
-train <- na.omit(train)
-
-#Aplicamos función propia de limpieza y tokenización
-#--->Train
-train <- train %>% mutate(texto_tokenizado = map(.x = text,
-                                                 .f = limpiar_tokenizar))
-train %>% select(texto_tokenizado) %>% head()
-train %>% slice(1) %>% select(texto_tokenizado) %>% pull()
-
-#--->Train
-test <- test %>% mutate(texto_tokenizado = map(.x = text,
-                                               .f = limpiar_tokenizar))
-test %>% select(texto_tokenizado) %>% head()
-test %>% slice(1) %>% select(texto_tokenizado) %>% pull()
-
-##--------------------Analisis Exploratorio------------------------------------#
-#Expansión ó unnest
-train_tidy <- train %>% select(-text) %>% unnest()
-train_tidy <- train_tidy %>% rename(token = texto_tokenizado)
-head(train_tidy)
-
-#Frecuencia de palabras
-
-train_tidy %>% group_by(name) %>% summarise(n = n()) 
-
-train_tidy %>%  ggplot(aes(x = name)) + geom_bar() + coord_flip() + theme_bw() 
-
-#palabras distintas por cada usuario
-
-train_tidy %>% select(name, token) %>% distinct() %>%  group_by(name) %>%
-  summarise(palabras_distintas = n()) 
-
-train_tidy %>% select(name, token) %>% distinct() %>%
-  ggplot(aes(x = name)) + geom_bar() + coord_flip() + theme_bw()
-
-#Palabras mas usadas por usuario
-
-train_tidy %>% group_by(name, token) %>% count(token) %>% group_by(name) %>%
-  top_n(10, n) %>% arrange(name, desc(n)) %>% print(n=30)
-
-##---------------------------------Stop Words---------------------------------##
-
-#Eliminamos Stop Words
-p_load(tm)
-head(stopwords('spanish'))
-
-train_tidy <- train_tidy  %>% 
-  anti_join(tibble(token =stopwords("spanish")))
-
-dim(train_tidy)
-#Verificamos de nuevo palabras mas usadas
-train_tidy %>% group_by(name, token) %>% count(token) %>% group_by(name) %>%
-  top_n(10, n) %>% arrange(name, desc(n)) %>% print(n=30)
-
-#Representación grafica de frecuencias
-
-train_tidy %>% group_by(name, token) %>% count(token) %>% group_by(name) %>%
-  top_n(10, n) %>% arrange(name, desc(n)) %>%
-  ggplot(aes(x = reorder(token,n), y = n, fill = name)) +
-  geom_col() +
-  theme_bw() +
-  labs(y = "", x = "") +
-  theme(legend.position = "none") +
-  coord_flip() +
-  facet_wrap(~name,scales = "free", ncol = 1, drop = TRUE)
-
-##---------------------------------Word Clouds--------------------------------##
-
-library(wordcloud)
-library(RColorBrewer)
-
-wordcloud_custom <- function(grupo, df){
-  print(grupo)
-  wordcloud(words = df$token, freq = df$frecuencia,
-            max.words = 400, random.order = FALSE, rot.per = 0.35,
-            colors = brewer.pal(8, "Dark2"))
-}
-
-df_grouped <- train_tidy %>% group_by(name, token) %>% count(token) %>%
-  group_by(name) %>% mutate(frecuencia = n / n()) %>%
-  arrange(name, desc(frecuencia)) %>% nest() 
-
-walk2(.x = df_grouped$name, .y = df_grouped$data, .f = wordcloud_custom)
-
-
-##---------------------------------MATRIZ--------------------------------##
-
-###CREAMOS MATRIZ UTILIZANDO COUNT VECTORIZER
-
-comentarios <- train_tidy$token
-
-#Quitamos números, signos de puntuación etc
-comentarios <- removeNumbers(comentarios)
-comentarios <- removePunctuation(comentarios)
-comentarios <- tolower(comentarios)
-comentarios <- stripWhitespace(comentarios)
-comentarios <- iconv(comentarios, from = "UTF-8", to="ASCII//TRANSLIT")
-
+comentarios<-train$text
 
 p_load("stringi")
-comentarios <- stri_trans_general(str = train$text, id = "Latin-ASCII")
+comentarios <- stri_trans_general(str = train$text, id = "Latin-ASCII") #elimnar tíldes
 comentarios[1]
+substr(comentarios, 1, 100)
+
+comentarios <- gsub('[^A-Za-z0-9 ]+', ' ', comentarios) #eliminamos todo lo que no sea alfanumérico
+substr(comentarios, 1, 100)
+
+
+comentarios <- tolower(comentarios)# Minúsculas
+substr(comentarios, 1, 100)
+
+
+comentarios <- gsub('\\s+', ' ', comentarios) # Espacios
+substr(comentarios, 1, 100)
+
+##Como nos interesan sólo las palabras elimnamos los números y espacios no simples
+comentarios <- gsub("\\d+", "", comentarios)
+comentarios <- gsub('\\s+', ' ', comentarios)
+comentarios <- trimws(comentarios)
+substr(comentarios, 1, 100)
+
+
 comentarios <- iconv(comentarios, from = "UTF-8", to="ASCII//TRANSLIT")
 comentarios[1]
+
+wordcloud(comentarios, max.words = 300, colors = brewer.pal(8, "Paired"))
+
+##-------------------------------------------------------------------------------
+#Convertimos comentarios en corpus
 
 texts<-VCorpus(VectorSource(comentarios))
 texts[1]
 
-texts<-tm_map(texts,content_transformer(tolower))
-texts<-tm_map(texts,content_transformer(removePunctuation))
-texts<-tm_map(texts,content_transformer(removeNumbers))
-texts<-tm_map(texts,content_transformer(tolower))
+#Eliminamos stopwords en español y elimanos las tildes de ese menú poque ya se las quité al mio
+stopwords_2 <- iconv(stopwords("spanish"), from = "UTF-8", to="ASCII//TRANSLIT")
+texts = tm_map (texts, removeWords, stopwords_2)
+
+## stemming
+
+texts = tm_map(texts, stemDocument, language = "spanish")
+
+wordcloud(texts, max.words = 300, colors = brewer.pal(8, "Paired"))
 
 
-p_load(stopwords)
-# Descargamos la lista de las stopwords en español de dos fuentes diferentes y las combinamos
-lista_palabras1 <- stopwords(language = "es", source = "snowball")
-lista_palabras2 <- stopwords(language = "es", source = "nltk")
-lista_palabras <- union(lista_palabras1, lista_palabras2)
-
-texts<-tm_map(texts,removeWords,lista_palabras)
-texts<-tm_map(texts,content_transformer(stripWhitespace))
-inspect(texts[1])
-
-matriz_1<-DocumentTermMatrix(texts)
-matriz_1
-inspect(matriz_1[1,])
-
+###CREAMOS MATRIZ---------------------------------------------------------------
+matriz_1 = DocumentTermMatrix(texts)
+inspect(matriz_1[1:100,])
 matriz_1 <- removeSparseTerms(matriz_1, sparse = 0.95)
-matriz_1
-inspect(matriz_1[1:2,])
+inspect(matriz_1[1:100,])
+
+##CONVERTIMOS LA MATRIZ EN DATA FRAME-------------------------------------------
+matriz_df = as.data.frame(as.matrix(matriz_1)) #convierte en data frame
+colnames(matriz_df) = make.names(colnames(matriz_df)) #variables R friendly
+matriz_df$name = train$name #añade la variable dependiente al set de datos
+
+## Porentaje Uribe - Lopez - Petro
+prop.table(table(matriz_df$name))
 
 
+### CREAMOS TRAIN Y TEST--------------------------------------------------------
+
+set.seed(123)
+train_index <- createDataPartition(matriz_df$name, p = .8)$Resample1
+train_df <- matriz_df[train_index,]
+test_df <- matriz_df[-train_index,]
+
+###MODELO RANDOM FOREST---------------------------------------------------------
+
+mtry_grid<-expand.grid(mtry =c(8,10,12))
+ctrl<- trainControl(method = "cv",
+                    number = 5)
 
 
+p_load(randomForest)
+set.seed(123)
+forest <- train(name~., 
+                data = train_df, 
+                method = "rf",
+                trControl = ctrl,
+                tuneGrid=mtry_grid,
+                metric="Accuracy",
+                ntree=200
+)
 
+forest
 
-
+plot(forest)
